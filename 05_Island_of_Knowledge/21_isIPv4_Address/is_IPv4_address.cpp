9c430e845c7f14e061ba85bb642676caf3e23e72
Matthew <82.matthew.jimenez@gmail.com>
2019-11-18 15:39:03 -0800

    begin to implement is_IPv4_address.cpp

--- 05_Island_of_Knowledge/21_isIPv4_Address/is_IPv4_address.cpp
+++ 05_Island_of_Knowledge/21_isIPv4_Address/is_IPv4_address.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main() {
 
+  string inputString {"172.16.254.1"}; // true
+  string inputString2 {"172.316.254.1"}; // true
+  string inputString3 {".254.255.0"}; // false
+  string inputString4 {"1.1.1.1a"}; // false
+  string inputString5 {"0..1.0.0"}; // false
 
+  cout << "inputString: " << inputString << endl;
+  
 
   return EXIT_SUCCESS;
 }
