209d1b6bbbeb29fd656e7285cdf788e31fa4299a
Matthew <82.matthew.jimenez@gmail.com>
2019-11-18 10:19:18 -0800

    add skeleton for 18_palindrome Rearranging

---
+++ 04_Exploring_the_Waters/18_Palindrome_Rearranging/palindrome_rearranging.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool palindromeRearranging(string inputString) {
+
+
+
+  return false;
+}
+
+int main() {
+
+  string inputString {"aabb"};
+  cout << "inputString: " << inputString << endl;
+  
+
+
+
+
+  return EXIT_SUCCESS;
+}
