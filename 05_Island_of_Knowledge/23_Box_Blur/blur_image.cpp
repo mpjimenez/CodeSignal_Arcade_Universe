0690f35b0cf3d7f56ad8d18a1baf05661083b4c8
Matthew <82.matthew.jimenez@gmail.com>
2019-11-20 10:28:47 -0800

    create skeleton for 23_box_blur

---
+++ 05_Island_of_Knowledge/23_Box_Blur/blur_image.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> boxBlur(vector<vector<int>> image) {
+  vector<vector<int>> blurred;
+
+
+
+
+
+
+  return blurred;
+}
+
+int main() {
+
+
+
+  return EXIT_SUCCESS;
+}
