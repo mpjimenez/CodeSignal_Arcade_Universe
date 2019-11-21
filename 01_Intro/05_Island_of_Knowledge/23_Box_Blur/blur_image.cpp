2195c675c5f521a5fb350abf50ac0b06f62d85bb
Matthew <82.matthew.jimenez@gmail.com>
2019-11-21 13:28:15 -0800

    implement box_blur.cpp

--- 01_Intro/05_Island_of_Knowledge/23_Box_Blur/blur_image.cpp
+++ 01_Intro/05_Island_of_Knowledge/23_Box_Blur/blur_image.cpp
@@ -17,17 +17,20 @@ vector<vector<int>> getDimensions(vector<vector<int>> image) {
   int size_of_col {0}; // the numbe of rows
 
   for (int row = 0; row < image.size(); row++) {
-    if (image[row].size() - row >= 3)
+    if (image.size() - row >= 3)
         size_of_row++;
     for (int col = 0; col < image[row].size(); col++) {
-      if (image[row].size() - row >= 3 && image.size() - col >= 3) {
+      if (image.size() - row >= 3 && image[row].size() - col >= 3) {
         size_of_col++;
       }
     }
   }
 
   size_of_col /= size_of_row;
-  vector<vector<int>> blurred (size_of_col, vector<int> (size_of_row, -1));
+  vector<vector<int>> blurred (size_of_row, vector<int> (size_of_col, -1));
+  cout << "number of rows: " << blurred.size() << endl;
+  cout << "number of cols: " << blurred[0].size() << endl;
+  print(blurred);
   return blurred;
 }
 
@@ -73,14 +76,33 @@ int main() {
                               {45, 18, 9, 0, 9, 18, 45},
                               {27, 81, 36, 63, 63, 72, 81}};
 
-/*
-  expected output:
-  { {39, 30, 26, 25, 31},
-    {34, 37, 35, 32, 32},
-    {38, 41, 44, 46, 42},
-    {22, 24, 31, 39, 45},
-    {37, 34, 36, 47, 59} }
-*/
+  /*
+    expected output:
+    {{39, 30, 26, 25, 31},
+     {34, 37, 35, 32, 32},
+     {38, 41, 44, 46, 42},
+     {22, 24, 31, 39, 45},
+     {37, 34, 36, 47, 59}};
+  */
+
+  vector<vector<int>> image5 {{172,14,85,186,28,155,122,39,44}, 
+                              {71,215,117,16,56,107,126,114,214}, 
+                              {83,173,135,218,151,200,133,58,8}, 
+                              {249,119,57,134,255,70,82,238,37}, 
+                              {199,76,110,36,121,185,187,82,92}, 
+                              {106,231,120,126,41,205,203,57,185}, 
+                              {246,241,81,146,148,46,16,128,233}};
+
+  /*
+    expected output:
+    {{118,128,110,124,119,117,95}, 
+    {135,131,126,134,131,125,112}, 
+    {133,117,135,152,153,137,101}, 
+    {140,112,111,130,149,145,129}, 
+    {156,129,103,117,128,123,131}};
+  
+  */
+
 
   // cout << "Unblurred image" << endl;
   // print(image1);
@@ -108,14 +130,21 @@ int main() {
   // print(blurred);
 
 
+  // cout << "Unblurred image" << endl;
+  // print(image4);
+  // cout << "image4.size() == number of rows: " << image4.size() << endl;
+  // cout << "image4[0].size() == number of columns " << image4[0].size() << endl << endl;
+  // vector<vector<int>> blurred = boxBlur(image4);
+  // cout << "Blurred Image:" << endl;
+  // print(blurred);
+
   cout << "Unblurred image" << endl;
-  print(image4);
-  cout << "image4.size() == number of rows: " << image4.size() << endl;
-  cout << "image4[0].size() == number of columns " << image4[0].size() << endl << endl;
-  vector<vector<int>> blurred = boxBlur(image4);
+  print(image5);
+  cout << "image5.size() == number of rows: " << image5.size() << endl;
+  cout << "image5[0].size() == number of columns " << image5[0].size() << endl << endl;
+  vector<vector<int>> blurred = boxBlur(image5);
   cout << "Blurred Image:" << endl;
   print(blurred);
-
 
 
   return EXIT_SUCCESS;
